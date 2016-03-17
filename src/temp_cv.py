80c671573793aa81fc6a317a16f7710534efce01
Refael Ackermann <me@refack.com>
2016-03-17 15:17:27 -0400

    Ver 3.0.0

--- src/temp_cv.py
+++ src/temp_cv.py
@@ -7,10 +7,13 @@ import io
 import cv2
 import numpy
 import os
+import sys
 from logging import FileHandler
 from vlogging import VisualRecord as _VisualRecord
 import scipy.ndimage as scind
 import logging
+from pydash import py_
+
 
 DILATOR_SIZE = 100
 MAX_HEIGHT = 40.0
@@ -19,7 +22,9 @@ ADJUSTED_HEIGHT = (MAX_HEIGHT - 1) * 1.4 - 2.0
 ADJUSTED_WIDTH = (MAX_WIDTH - 1) * 8.0 - 2.0
 DEFECT_MIN_SIZE = 2
 RECT_SUB_PIX_PADDING = 20
-
+K1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+K_CIRC_3 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+K_CIRC_5 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
 
 class VisualRecord(_VisualRecord):
     def __init__(self, title, imgs, footnotes="", fmt="webp"):
@@ -33,8 +38,9 @@ class VisualRecord(_VisualRecord):
             imgs = [cv2.resize(img.astype(numpy.uint8), None, fx=2, fy=2) for img in imgs]
 
         if multi:
-            if max(imgs[0].shape[:2]) > 1000:
-                fact = 1.0 / len(imgs)
+            max_w = imgs[0].shape[1]
+            if max_w > 1900:
+                fact = 1.0 / len(imgs) * (1900.0 / max_w)
                 imgs = [cv2.resize(img.astype(numpy.uint8), None, fx=fact, fy=fact) for img in imgs]
         else:
             pass
@@ -44,12 +50,20 @@ class VisualRecord(_VisualRecord):
 
 
 class Helper(object):
-    time_stamp = str(time.time())
+    time_stamp = format(int((time.time() * 10) % 10000000), "6d")
     logger = logging.getLogger("cfu4you")
     htmlfile = 'cfu4you' + time_stamp + '.html'
     fh = FileHandler(htmlfile, mode="w")
     fh._old_close = fh.close
-    fh.close = lambda h=htmlfile, fh=fh: (webbrowser.open_new_tab(h), fh._old_close())
+
+    def on_log_close(h=htmlfile, fh=fh):
+        if 'last_type' in sys.__dict__:
+            print '/'.join(["file:/", os.getcwd().replace('\\', '/'), h])
+        else:
+            webbrowser.open_new_tab(h)
+        fh._old_close()
+
+    fh.close = on_log_close
     fh.propagate = False
     logger.setLevel(logging.DEBUG)
     logger.addHandler(fh)
@@ -94,6 +108,7 @@ class Helper(object):
     def log(cls, msg, imgs, *args):
         cls.logText(msg, args)
         cls.logger.debug(VisualRecord("", imgs))
+        cls.fh.flush()
 
     @classmethod
     def equalizeMulti(cls, img):
@@ -154,21 +169,26 @@ def preprocess(img, block_size):
 
 
 def blowup(img):
-    pre = img
-    split = mycv2.split(img)
-    clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(10, 10))
-    b_split = [clahe.apply(c) for c in split]
-    img = cv2.merge(b_split)
-    l = mycv2.cvtColor(img, cv2.COLOR_BGR2HLS_FULL)[:, :, 1]
-    l = deluminate(l)
-    per = numpy.percentile(l, 90).astype(int)
+    img = mycv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h,s,v = mycv2.split(img)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(10, 10))
+    h = clahe.apply(h)
+    v = clahe.apply(v)
+    s = clahe.apply(cv2.multiply(s, 2))
+    img = mycv2.cvtColor(cv2.merge([h,s,v]), cv2.COLOR_HSV2BGR)
+    per = int(numpy.percentile(v, 90))
     Helper.logText("per {0:d}".format(per))
-    th, mask = cv2.threshold(l, per, 255, cv2.THRESH_BINARY)
-    mask2 = cv2.erode(mask, None, iterations=4)
-    mask3 = cv2.dilate(mask2, None, iterations=7)
-    l2 = cv2.multiply(l, mask3 / 255)
-    Helper.log('blowup', [pre, img, l, mask, mask2, mask3, l2, cv2.subtract(l, l2)])
-    return l2, img, mask
+    ADPT_BLOCK_SIZE = 51
+    threshold = mycv2.adaptiveThreshold(v, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, ADPT_BLOCK_SIZE, per/255.0)
+    eroded = mycv2.erode(threshold, K_CIRC_5, iterations=2)
+    dilated = mycv2.dilate(eroded, K_CIRC_5, iterations=2)
+    masked = mycv2.multiply(v, dilated / 255)
+    masked_eq = cv2.equalizeHist(masked)
+    d = mycv2.subtract(img, cv2.merge([masked, masked, masked]))
+    Helper.log('d', [d])
+    Helper.log('v, threshold, eroded, dilated', [v, threshold, eroded, dilated])
+    Helper.log('img, masked, threshold', [img, masked, eroded])
+    return img, masked_eq, eroded
 
 
 def deluminate(img, rad=70):
@@ -185,10 +205,10 @@ def deluminate(img, rad=70):
     avg_image = preprocess(smoothed_image, rad)
     smoothed_image2 = mycv2.blur(avg_image, (krad, krad))
     delumi = mycv2.subtract(img, smoothed_image2, dtype=cv2.CV_8U)
-    delumi[delumi < 48] = 0
     normalized = mycv2.equalizeHist(delumi)
     Helper.log('deluminate', [img, smoothed_image, avg_image, smoothed_image2, delumi, normalized])
     return normalized
+
 
 
 def findROIs(img):
@@ -214,16 +234,19 @@ def findROIs(img):
     return ret
 
 
-def cropROI(img, ROI):
-    info = img.copy()
-    mycv2.circle(info, ROI['c'], ROI['r'], (0, 255, 0), thickness=4)
-    clip = mycv2.getRectSubPix(img, (2 * ROI['r'], 2 * ROI['r']), ROI['c'])
-    color = 1 if len(img.shape) < 3 else (1, 1, 1)
-    neg = clip.copy()
-    mycv2.circle(neg, (ROI['r'], ROI['r']), ROI['r'], color, -1)
-    cropped2 = mycv2.subtract(clip, neg)
-    Helper.log("crop", [info, clip, neg, cropped2])
-    return cropped2
+def cropROI(ROI, *imgs):
+    out = []
+    for img in imgs:
+        info = img.copy()
+        mycv2.circle(info, ROI['c'], ROI['r'], (0, 255, 0), thickness=4)
+        clip = mycv2.getRectSubPix(img, (2 * ROI['r'], 2 * ROI['r']), ROI['c'])
+        color = 1 if len(img.shape) < 3 else (1, 1, 1)
+        neg = clip.copy()
+        mycv2.circle(neg, (ROI['r'], ROI['r']), ROI['r'], color, -1)
+        cropped = mycv2.subtract(clip, neg)
+        out.append(cropped)
+    Helper.log("crop", [info, clip, neg, cropped])
+    return out
 
 
 class ContourStats(object):
@@ -240,8 +263,6 @@ class ContourStats(object):
         self.regularity = regularity
         self.area = area
         self.rect = rect
-        self.mw = rect[1][0]
-        self.mh = rect[1][1]
         self.perimeter = perimeter
         self.hull = hull
         self.defects = defects
@@ -263,68 +284,73 @@ class ContourStats(object):
         return state
 
     @classmethod
-    def from_contour(cls, cnt):
+    def from_contour(cls, cnt, *args):
         if len(cnt) < 5:
             return None
         area = cv2.contourArea(cnt)
-        if area == 0: return None
+        if area < 1:
+            return None
+        rect = cv2.fitEllipse(cnt)
+        mw = rect[1][0]
+        mh = rect[1][1]
+        rect_size = mw * mh
+        if rect_size <= 0:
+            return None
+
         M = cv2.moments(cnt)
-        if M['m00'] == 0: return None
+        if M['m00'] == 0:
+            return None
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
+        x, y, w, h = cv2.boundingRect(cnt)
         perimeter = cv2.arcLength(cnt, True)
         hull = cv2.convexHull(cnt)
         hullPoints = cv2.convexHull(cnt, returnPoints=0)
         defects = cv2.convexityDefects(cnt, hullPoints) if (len(hullPoints) > 2) else [[]]
-        rect = cv2.fitEllipse(cnt)
-        x, y, w, h = cv2.boundingRect(cnt)
-        mw = rect[1][0]
-        mh = rect[1][1]
-        size = mw * mh
-        if size <= 0: return None
-        area_circularity = (size * math.pi) / (area * 4.0)
+        area_circularity = (rect_size * math.pi) / (area * 4.0)
         roundness = abs(math.log(area_circularity, 1.1))
         rect_proportion = mw / mh
         regularity = abs(math.log(rect_proportion, 10))
-        ret = cls(contour=cnt, M=M, cx=cx, cy=cy, offset=(-x, -y), width=w, height=h, roundness=roundness,
+        ret = cls(cnt, M=M, cx=cx, cy=cy, offset=(-x, -y), width=w, height=h, roundness=roundness,
                   regularity=regularity, area=area, rect=rect, perimeter=perimeter, hull=hull, defects=defects)
         return ret
 
 
     @classmethod
-    def from_contours(cls, contours):
-        return map(cls.from_contour, contours)
+    def find_contours(cls, img, predicate=None, inner=True):
+
+        ret = cv2.findContours(img.copy(), cv2.CHAIN_APPROX_SIMPLE, cv2.RETR_LIST)
+        img_marked, contours, [hierarchy] = ret
+        stats = py_(hierarchy)\
+            .map(lambda x, i: contours[i] if (hierarchy[i][3] == -1 or inner) else [])\
+            .map(cls.from_contour)\
+            .compact()\
+            .filter(predicate)\
+            .value()
+        Helper.logText("got {0} markers out of {1} raw", len(stats), len(hierarchy))
+        return stats
 
 
-def find_colonies1(roi, roi_color):
-    k3 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-    _, threshold = mycv2.threshold(roi, 32, 255, mycv2.THRESH_BINARY)
-    morphology = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, k3, iterations=3)
-    Helper.log('threshold, morphology', [threshold, morphology])
-    dt = mycv2.distanceTransform(morphology, cv2.DIST_L1, cv2.DIST_MASK_3).astype(numpy.uint8)
-    _, dt_trash = mycv2.threshold(dt, 6, 255, mycv2.THRESH_BINARY)
-    Helper.log('morphology, distanceTransform', [morphology, dt_trash])
-    __, pre_contours, ___ = mycv2.findContours(dt_trash, cv2.CHAIN_APPROX_NONE, cv2.RETR_LIST)
-    stats = filter(lambda s: s is not None and s.area < 5000, ContourStats.from_contours(pre_contours))
-    markers = numpy.zeros(roi.shape, dtype=numpy.int32)
+def find_colonies1(masked, color):
+    stats = ContourStats.find_contours(masked, lambda r: (r.roundness < 4) and (4 < r.area < 16000))
+    markers = numpy.zeros(masked.shape, dtype=numpy.int32)
     for i, s in enumerate(stats, 1):
-        cv2.drawContours(markers, [s.contour], -1, i, thickness=-1)
+        cv2.drawMarker(markers, (s.cx, s.cy), i, markerType=cv2.MARKER_SQUARE, markerSize=2)
 
-    Helper.log('roi_color, markers', [roi_color, markers])
-    ws = mycv2.watershed(roi_color, markers)
-    ws_tr = mycv2.threshold(ws.astype(numpy.uint8), 1, 255, mycv2.THRESH_BINARY)
-    Helper.log('distanceTransform, ws', [dt_trash, ws_tr])
-
-    dilated = mycv2.dilate(dt_trash, k3, iterations=4)
-    Helper.log('distanceTransform, dilated', [dt_trash, dilated])
-
-    clean = ws_tr
-    __, contours, ___ = mycv2.findContours(clean, cv2.CHAIN_APPROX_NONE, cv2.RETR_LIST)
-    stats = filter(None, ContourStats.from_contours(contours))
-    Helper.logText('got stats')
-    stats = filter(lambda r: r.roundness < 2 and r.area > 12, stats)
+    markers = mycv2.watershed(color, markers)
+    masked2 = masked.copy()
+    masked2[markers == -1] = 0
+    _, mask_th = cv2.threshold(masked2, 1, 255, cv2.THRESH_BINARY)
+    stats = ContourStats.find_contours(mask_th, lambda r: (r.roundness < 4) and (4 < r.area < 16000))
     stats.sort(key=lambda r: r.cy)
-    Helper.logText('filtered stats')
+    contours = numpy.zeros(markers.shape, dtype=numpy.uint8)
+    for i, s in enumerate(stats):
+        cv2.drawContours(contours, [s.contour], -1, 7 * ((i % 64) + 1), thickness=cv2.FILLED, lineType=cv2.LINE_8)
+        cv2.drawContours(contours, [s.contour], -1, 255, thickness=1, lineType=cv2.LINE_8)
+    color2 = color.copy()
+    color2[markers == -1] = (0, 0, 255)
+    Helper.log('markers', cv2.merge([masked2, mask_th, contours]))
+    Helper.log('contours', [color2])
     return stats
 
 
@@ -347,6 +373,10 @@ def churn(roi, stats, filename):
     worksheet.set_column(7, 8, MAX_WIDTH)
     Helper.logText("prepare excel")
 
+    cp = roi.copy()
+    cv2.drawContours(cp, map(lambda s:s.contour, stats), -1, (0, 255, 0))
+    cv2.imwrite(OUTPUT_DIR + '\\markers2.png', cp)
+
     red2 = []
     green2 = []
     """s:type : CntStats"""
@@ -359,7 +389,7 @@ def churn(roi, stats, filename):
             s.cx - (int(math.log(i, 10)) + 1) * 8,
             s.cy - (s.height / 2) - 2
         )
-        cv2.putText(roi_marked, str(i), label_point, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), thickness=2)
+        cv2.putText(roi_marked, str(i), label_point, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), thickness=2)
         slc = cv2.getRectSubPix(img_eq, (s.width + RECT_SUB_PIX_PADDING, s.height + RECT_SUB_PIX_PADDING), (s.cx, s.cy))
         slc_m = slc.copy()
         one_side_padding = RECT_SUB_PIX_PADDING / 2
@@ -445,14 +475,45 @@ def churn(roi, stats, filename):
     Helper.logText(u"saved excel {0:s}".format(filename))
 
     # markers_ws = mycv2.watershed(roi_color, markers)
-    Helper.logText(u"raw = {0:d}\t\tnumber = {1:d}".format(len(stats), len(green2)))
+    Helper.logText(u"churn: raw {0:d}\t\tfinal {1:d}".format(len(stats), len(green2)))
     # cols = cv2.watershed(roi_color, dt_trash)
 
     return roi_marked
 
 
+def process_file(file):
+    Helper.logText("*************** {0:s} ***************".format(file))
+    PATH_NAME = os.path.join(DIR_NAME, file)
+    FILE_NAME, _ext = os.path.splitext(file)
+    new_name = '.'.join([FILE_NAME, "_{0:4d}".format(int(time.time()) % 10000), 'xlsx'])
+    filename = os.path.join(OUTPUT_DIR, new_name)
+    orig = mycv2.imread(PATH_NAME)
+    if orig.shape[0] < orig.shape[1]:
+        orig = numpy.rot90(orig, 3)
+        Helper.logText("rotated")
+    blown, masked, big_blobs = blowup(orig)
+    rois = findROIs(big_blobs)
+    roi = rois[0]
+    roi_masked, roi_color  = cropROI(roi, masked, blown)
+    # roi2 = cv2.getRectSubPix(roi_color, (100, 100), (1026, 1255))
+    # roihsl = cv2.cvtColor(roi2, cv2.COLOR_BGR2HLS_FULL)
+    # split = cv2.split(roihsl)
+    # for sat in split:
+    #     sat1 = Helper.equalizeMulti(sat)
+    #     slices = [cv2.threshold(sat1, i, 255, cv2.THRESH_BINARY)[1] for i in range(192, 256)]
+    #     Helper.log("roi2, sat, sat1...", [roi2, sat, sat1] + slices)
+    # roi_masked = masked
+    # roi_color = orig
+    st = find_colonies1(roi_masked, roi_color)
+    # data = [s.__getstate__() for s in st]
+    # df = pandas.DataFrame(data)
+    # df.to_csv(filename + '.csv')
+    colonies1_merged = churn(roi_color, st, filename)
+    Helper.log(PATH_NAME, colonies1_merged)
 
-OUTPUT_DIR = r"C:\code\6broad\colony-profile\output\cfu4good\RB"
+
+
+
 # PATH_NAME = r"V:\Camera\5\IMG_20151125_182927.jpg" # single plate (on stand)
 # PATH_NAME = r"V:\Camera\5\IMG_20151125_183446.jpg"  # two plates (left one cut)
 # PATH_NAME = r"V:\Camera\1\IMG_20151104_165813.jpg"  # tiny plate
@@ -485,53 +546,12 @@ OUTPUT_DIR = r"C:\code\6broad\colony-profile\output\cfu4good\RB"
 DIR_NAME = r"V:\CFU\RB\images_for_Refael"
 # files = os.listdir(DIR_NAME)
 # random.shuffle(files)
-files = ['E072_g7.JPG']
+# files = ['E072_g7.JPG'] # HARD
+files = ['E072_d7.JPG'] # EASY
 
-def method_name(tag, img, roi3):
-    roi32 = Helper.equalizeMulti(roi3)
-    img = numpy.absolute(img).astype(numpy.uint8)
-    sobelxc = cv2.split(img)
-    sobelx1 = Helper.equalizeMulti(img)
-    sobelx2 = reduce(lambda s, c: cv2.min(s, c), sobelxc, cv2.multiply(numpy.ones(img.shape[:2], dtype=numpy.uint8), 255))
-    sobelx2 = Helper.equalizeMulti(sobelx2)
-    sobelx3 = reduce(lambda s, c: cv2.max(s, c), sobelxc, numpy.ones(img.shape[:2], dtype=numpy.uint8))
-    sobelx3 = Helper.equalizeMulti(sobelx3)
-    Helper.log(tag + ": orig, origeq, [eq, cast, eq2m, min, max]", [roi3, roi32] + [Helper.equalizeMulti(c) for c in sobelxc] + [img, sobelx1, sobelx2, sobelx3])
+OUTPUT_DIR = r"C:\code\6broad\colony-profile\output\cfu4good\RB"
 
 
-for f in files:
-    Helper.logText("*************** {0:s} ***************".format(f))
-    PATH_NAME = os.path.join(DIR_NAME, f)
-    FILE_NAME, _ext = os.path.splitext(f)
-    new_name = '.'.join([FILE_NAME, "_{0:4d}".format(int(time.time()) % 10000), 'xlsx'])
-    filename = os.path.join(OUTPUT_DIR, new_name)
-
-    orig = mycv2.imread(PATH_NAME)
-    if orig.shape[0] < orig.shape[1]:
-        orig = numpy.rot90(orig, 3)
-        Helper.logText("rotated")
-    blown_bw, blown_color, hi_cont = blowup(orig)
-    Helper.log('blowup', [blown_bw, blown_color, hi_cont])
-    rois = findROIs(hi_cont)
-    roi = rois[0]
-    roi_color = cropROI(orig, roi)
-    roi_hi_cont = cropROI(hi_cont, roi)
-
-    # roi2 = cv2.getRectSubPix(roi_color, (100, 100), (1026, 1255))
-    # roihsl = cv2.cvtColor(roi2, cv2.COLOR_BGR2HLS_FULL)
-    # split = cv2.split(roihsl)
-    # for sat in split:
-    #     sat1 = Helper.equalizeMulti(sat)
-    #     slices = [cv2.threshold(sat1, i, 255, cv2.THRESH_BINARY)[1] for i in range(192, 256)]
-    #     Helper.log("roi2, sat, sat1...", [roi2, sat, sat1] + slices)
-
-    # roi_hi_cont = hi_cont
-    # roi_color = orig
-
-    st = find_colonies1(roi_hi_cont, roi_color)
-    # data = [s.__getstate__() for s in st]
-    # df = pandas.DataFrame(data)
-    # df.to_csv(filename + '.csv')
-    colonies1_merged = churn(roi_color, st, filename)
-    Helper.log(PATH_NAME , colonies1_merged)
+for f1 in files:
+    process_file(f1)
 
