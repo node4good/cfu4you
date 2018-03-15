245b5d3976b67184bdeccffb0b38a74bf03b493e
Refael Ackermann <refack@gmail.com>
2018-03-15 15:55:55 -0400

    With many stats

--- src/batch_ilum.py
+++ src/batch_ilum.py
@@ -3,66 +3,97 @@ from collections import namedtuple
 from datetime import datetime
 import glob
 import re
-from itertools import groupby, takewhile
 
 import numpy as np
 import pydash
 # from skimage import img_as_float, img_as_ubyte, img_as_uint
 # from skimage.exposure import rescale_intensity
-from skimage.exposure import rescale_intensity
+from skimage.exposure import rescale_intensity, histogram
 from skimage.filters import median
 # from skimage.filters.rank import maximum, mean
 from skimage.io import imread, imsave
 from skimage.morphology import disk
 from skimage.transform import rescale
-import skimage.io._plugins.tifffile_plugin as tiff
 
 from LogHelper import LogHelper
 
 ROOT_DIR = r"T:\NewImaging"
 
-m = {'w1': 250, 'w2': 3000}
-
-
 NAME_PARSER = re.compile('^([^_]+)_([A-G])(\d\d)_s(\d{1,2})(?:_w(\d))?')
-Frame = namedtuple('Frame', 'filename, experiment, row, column, site, wave')
+Frame = namedtuple('Frame', 'fullpath, filename, experiment, row, column, site, wave')
 
 
 def parse_file_name(f):
-    parts = [f] + NAME_PARSER.split(f)[1:6]
+    n = os.path.basename(f)
+    parts = [f, n] + NAME_PARSER.split(n)[1:6]
     return Frame._make(parts)
 
 
 def get_plate_files(plate):
-    # example "T:\NewImaging\2-7-18 T0-48in T0-48 24in pTiGc-20\2018-02-17\59574"
-    plates = glob.glob(ROOT_DIR + "\\*\\*\\*\\")
-    plate = pydash.find(plates, lambda p: plate in p)
-    files = glob.glob1(plate, "*.tif")
+    # example "ROOT_DIR\2-7-18 T0-48in T0-48 24in pTiGc-20\2018-02-17\59574"
+    dirs = glob.glob(ROOT_DIR + "\\*\\*\\*\\")
+    plate = pydash.find(dirs, lambda p: plate in p)
+    files = glob.glob(plate + "\\*.tif")
     parsed = map(parse_file_name, files)
-    grouped = [(k, tuple(g)) for k, g in groupby(parsed, lambda p: p['id'])]
-    ws = len(grouped[0][1])
-    assert(all([len(g) == ws for k, g in grouped]))
-    return plate
+    return parsed
 
 
-def calculate_ilum():
-    plate = get_plate_files("59798")
+def calculate_stats():
+    parsed = get_plate_files("59798")
     ts = "{:%Y-%m-%d-%H-%M-%S}".format(datetime.now())
     for w in ['w2']:
-        m1 = m[w]
-        all_files = glob.glob(plate + "\\*" + w + "*.tif")
+        all_files = filter(lambda f: f.wave == w[1], parsed)
         accum = np.zeros((2160, 2160), dtype=np.uint32)
         # files = filter(lambda x: 's1' not in x and 's7' not in x, all_files)
         files = all_files
         nof = len(all_files)
         gmn, gmx = 2**15, 0
-        for i, file_path in enumerate(files, 1):
-            filename = os.path.basename(file_path)
-            img = imread(file_path)
-            mn, md, th, mx = np.percentile(img, (2, 50, 90, 98))
+        for i, frame in enumerate(files, 1):
+            img = imread(frame.fullpath)
+            s = np.sort(img.flatten())
+            LogHelper.logText(str(np.argwhere(img == img.max())))
+            LogHelper.logText(','.join(map(str, s[-50:-1])))
+            mn, mx = np.percentile(img, (0.1, 99.9))
             gmn = min(mn, gmn)
             gmx = max(mx, gmx)
-            LogHelper.logText('%3d of %d, %4d-%4d-%4d-%4d, %4d:%4d' % (i, nof, mn, md, th, mx, img.min(), img.max()))
+            LogHelper.logText(
+                '%3d of %d, %4d-%4d-%4d-%5d, %.0f-%.0f'
+                % (i, nof, img.min(), mn, mx, img.max(), np.mean(img), np.std(img))
+            )
+            clip = img
+            accum += clip
+        LogHelper.logText('w %f,%f' % (gmn, gmx))
+        filename = os.path.join(ROOT_DIR, w + '-' + ts + '-%s.tiff')
+        LogHelper.logText(filename)
+        # np.save(filename % 'accum', accum)
+        avg_u = (accum / len(files)).astype(np.uint16)
+        imsave(filename % 'avg_u', avg_u)
+        smooth = median(avg_u, disk(20))
+        imsave(filename % 'smooth', smooth)
+
+
+def calculate_ilum():
+    parsed = get_plate_files("59798")
+    ts = "{:%Y-%m-%d-%H-%M-%S}".format(datetime.now())
+    for w in ['w2']:
+        all_files = filter(lambda f: f.wave == w[1], parsed)
+        accum = np.zeros((2160, 2160), dtype=np.uint32)
+        # files = filter(lambda x: 's1' not in x and 's7' not in x, all_files)
+        files = all_files
+        nof = len(all_files)
+        gmn, gmx = 2**15, 0
+        for i, frame in enumerate(files, 1):
+            img = imread(frame.fullpath)
+            s = np.sort(img.flatten())
+            LogHelper.logText(str(np.argwhere(img == img.max())))
+            LogHelper.logText(','.join(map(str, s[-50:-1])))
+            mn, mx = np.percentile(img, (0.1, 99.9))
+            gmn = min(mn, gmn)
+            gmx = max(mx, gmx)
+            LogHelper.logText(
+                '%3d of %d, %4d-%4d-%4d-%5d, %.0f-%.0f'
+                % (i, nof, img.min(), mn, mx, img.max(), np.mean(img), np.std(img))
+            )
             # clip = np.clip(img, 0, m1)
             clip = img
             accum += clip
@@ -130,12 +161,10 @@ def find_min():
         LogHelper.logText("*************** {0} - {1} ***************".format(m, ts))
 
 
-DIM = 1080
-sDIM = 216 + 4
-DIMP = sDIM + 5
-
-
 def stitch():
+    DIM = 1080
+    sDIM = 216 + 4
+    DIMP = sDIM + 5
     plate_name = '59476'
     plates = glob.glob(r"T:\NewImaging\ref-data\exp1\*")
     plate = filter(lambda d: plate_name in d, plates).pop()
